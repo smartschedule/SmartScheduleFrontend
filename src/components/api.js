491e61c598eb4e01008820f2277401aee8032bec
Adam Bajguz <adam.bajguz@gmail.com>
2019-06-09 15:59:52 +0200

    Fix

--- src/components/api.js
+++ src/components/api.js
@@ -59,15 +59,15 @@ export const unblockUser = async id =>
   });
 
 export const createCalendar = (name, colorHex, userId) =>
-  api.post('CreateCalendar', { name, colorHex, userId });
+  api.post('calendar/create', { name, colorHex, userId });
 
 export const deleteCalendar = async id =>
-  await api.post('DeleteCalendar', { id });
+  await api.post('calendar/delete', { id });
 export const getCalendars = async () =>
   (await api.get('calendars')).data.calendars;
 
 export const updateCalendar = async (id, name) =>
-  await api.post('UpdateCalendar', { id, name, colorHex: '#ffff' });
+  await api.post('calendar/update', { id, name, colorHex: '#ffff' });
 
 export const getEvents = async () => (await api.get('events')).data.events;
 
@@ -81,7 +81,7 @@ export const createEvent = async (
   longitude,
   latitude,
 ) => {
-  await api.post('CreateEvent', {
+  await api.post('event/create', {
     startDate,
     endTime,
     reminderAt,
@@ -92,4 +92,4 @@ export const createEvent = async (
     latitude,
   });
 };
-export const deleteEvent = async id => await api.post('DeleteEvent', { id });
+export const deleteEvent = async id => await api.post('event/delete', { id });
